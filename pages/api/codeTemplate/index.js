4c4c29110a0ca262a9f94a5065032180787bb754
eric-qli <92590396+eric-qli@users.noreply.github.com>
2024-11-01 20:32:15 -0400

    update

--- pages/api/codeTemplate/index.js
+++ pages/api/codeTemplate/index.js
@@ -3,16 +3,16 @@ import prisma from "@/utils/db";
 export default async function handler(req, res) {
     console.log('Im here');
     const authHeader = req.headers.authorization;
+    
+    if (!authHeader) {
+        return res.status(401).json({ error: 'Unauthorized: No token provided' });
+    }
 
     try {
         // Save a new template (POST /templates)
         if (req.method === 'POST') {
             const { title, description, tags, code, language, authorId } = req.body;
             
-            if (!authHeader) {
-                return res.status(401).json({ error: 'Unauthorized: No token provided' });
-            }
-
             const template = await prisma.codeTemplate.create({
                 data: {
                     title,
