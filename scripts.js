3456fb2355579429b9c614b3d68970b27e1947bc
eric-qli <92590396+eric-qli@users.noreply.github.com>
2024-11-28 12:14:01 -0500

    added e11

---
+++ scripts.js
@@ -0,0 +1,12 @@
+function getBaseUrl() {
+    return "https://www.ericqli.ca";
+  }
+  
+  function getUTorId() {
+    return 1007654307;
+  }
+  
+  module.exports = {
+    getBaseUrl,
+    getUTorId,
+  };
